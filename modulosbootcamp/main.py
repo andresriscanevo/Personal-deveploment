550bff5d97d9bebaf35a275127bafde31b557558
andres riscanevo <andresriscanevo10@gmail.com>
2022-06-04 22:59:14 -0500

    "modulos openbootcamp"

---
+++ modulosbootcamp/main.py
@@ -0,0 +1,11 @@
+from this import d
+import modulooperaciones as operacion
+def main():
+    sum=operacion.suma(2,4,5,6)
+    res=operacion.resta(20,4)
+    mul=operacion.multiplicacion(10,2)
+    div=operacion.division(120,2)
+    print(sum,res,mul,div)
+if __name__=='__main__':
+    main()
+
---
+++ modulosbootcamp/modulooperaciones.py
@@ -0,0 +1,13 @@
+
+def suma(*args):
+    sum=0
+    for i in args:
+        sum += i
+    return sum
+
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b
+def division(a,b):
+    return a/b
